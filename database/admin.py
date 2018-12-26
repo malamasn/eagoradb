f74e3ec405b03376569772e9f7122a81c6b32ab7
Nikolas Malamas <malamasn@hotmail.com>
2018-12-26 20:39:07 +0200

    Use ModelAdmin to show models in columns in admin site

--- database/admin.py
+++ database/admin.py
@@ -3,9 +3,28 @@ from .models import *
 
 admin.site.site_header = 'E Agora DB'
 
-admin.site.register(Product)
-admin.site.register(Store)
-admin.site.register(Supplier)
-admin.site.register(Selling)
-admin.site.register(Orders)
-admin.site.register(Has)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type')
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Selling)
+class SellingAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'store_id', 'price')
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'store_id', 'client')
+
+@admin.register(Has)
+class HasAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'order_id', 'quantity')
