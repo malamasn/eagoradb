430bd181cc460f0bfaf0162f4ae6475faf8428f4
Nikolas Malamas <malamasn@hotmail.com>
2018-12-28 00:02:48 +0200

    Add selling products at store template

--- database/views.py
+++ database/views.py
@@ -49,8 +49,18 @@ class StoreView(generic.ListView):
     def get(self, request, pk):
         store = get_object_or_404(Store, pk=pk)
         form = StoreForm(instance = store)
+        sells = Selling.objects.filter(store_id = pk)
+        product_names = list()
+        price_list = list()
+        for i in sells:
+            print(i.product_id.name, i.price)
+        #     product = Product.objects.filter(pk = i.product_id)
+        #     product_names.append(product.name)
+        #     price_list.append(i.price)
 
-        args = {'store': store, 'form': form}
+        args = {'store': store, 'form': form,
+                'product_names': product_names, 'price_list': price_list,
+                'sells': sells}
         return render(request, self.template_name, args)
 
 
