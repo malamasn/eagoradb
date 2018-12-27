804199da8ff084b6611b481662ec67aa58546b81
Nikolas Malamas <malamasn@hotmail.com>
2018-12-28 00:02:09 +0200

    Omit from home page url the home string

--- database/urls.py
+++ database/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'database'
 urlpatterns = [
-    path('home/', views.HomeView.as_view(), name = 'home'),
+    path('', views.HomeView.as_view(), name = 'home'),
     path('find_products/', views.FindProducts.as_view(), name = 'find_products'),
     path('find_stores/', views.FindStores.as_view(), name = 'find_stores'),
     path('product_<int:pk>/', views.ProductView.as_view(), name = 'product'),
